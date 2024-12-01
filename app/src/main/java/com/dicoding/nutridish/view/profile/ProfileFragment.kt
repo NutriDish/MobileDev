6582b9c0042520dc41a240f5236c2c1cde489e10
Ridhuan Rangga Kusuma <ridhuankeren@gmail.com>
2024-12-01 21:19:21 +0700

    About and Update

--- app/src/main/java/com/dicoding/nutridish/view/profile/ProfileFragment.kt
+++ app/src/main/java/com/dicoding/nutridish/view/profile/ProfileFragment.kt
@@ -1,11 +1,14 @@
 package com.dicoding.nutridish.view.profile
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.dicoding.nutridish.R
+import com.dicoding.nutridish.view.profile.settings.AboutActivity
+import com.dicoding.nutridish.view.profile.settings.UpdateProfileActivity
 
 
 class ProfileFragment : Fragment() {
@@ -19,5 +22,19 @@ class ProfileFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val buttonUpdate = view.findViewById<View>(R.id.btnUpdateProfile)
+        val buttonAbout = view.findViewById<View>(R.id.btnAboutApp)
+
+        buttonAbout.setOnClickListener{
+            val intent = Intent(activity, AboutActivity::class.java)
+            startActivity(intent)
+        }
+        buttonUpdate.setOnClickListener{
+            val intent = Intent(activity, UpdateProfileActivity::class.java)
+            startActivity(intent)
+        }
+    }
 
 }
